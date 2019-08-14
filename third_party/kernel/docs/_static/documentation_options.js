dda544a5770ab27704bb59a95b19690d4ec4e366
Brendan Higgins <brendanhiggins@google.com>
2019-08-13 22:58:09 -0700

    third_party: kernel: updated docs

--- third_party/kernel/docs/_static/documentation_options.js
+++ third_party/kernel/docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.2.0-00018-g0442cbdc4ea52',
+    VERSION: '5.3.0-rc3+',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
